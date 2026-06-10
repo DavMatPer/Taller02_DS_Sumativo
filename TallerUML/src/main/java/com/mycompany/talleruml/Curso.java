1e116e8c96f8acfe2e83c6daef40a1b4b4f15fed
David Matos Pérez <davmatos@espol.edu.ec>
2026-06-10 14:23:37 -0500

    Agregado constructor y atributos a curso

--- TallerUML/src/main/java/com/mycompany/talleruml/Curso.java
+++ TallerUML/src/main/java/com/mycompany/talleruml/Curso.java
@@ -1,10 +1,31 @@
 package com.mycompany.talleruml;
 
+import java.util.ArrayList;
+
 public class Curso {
-    private ActividadesSumativas actividad_sumativa;
+    private ArrayList<ActividadesSumativas> actividad_sumativa;
     private String id;
     private String nombre;
     private Boolean estadoOperativo;
+    private Profesor profesor;
+    private ArrayList<Estudiante> estudiantesEnEspera;
+    private ArrayList<Estudiante> estudiantesInscritos;
+    private ArrayList<Foro> foro;
+    
+
+    public Curso(ArrayList<ActividadesSumativas> actividad_sumativa, String id, String nombre, Boolean estadoOperativo, Profesor profesor) {
+        this.actividad_sumativa = actividad_sumativa;
+        this.id = id;
+        this.nombre = nombre;
+        this.estadoOperativo = estadoOperativo;
+        this.profesor = profesor;
+        
+        foro = new ArrayList<>();
+        estudiantesEnEspera = new ArrayList<>();
+        estudiantesInscritos = new ArrayList<>();
+    }
+    
+    
 
     public ActividadesSumativas getActividad_sumativa() {
         return actividad_sumativa;
