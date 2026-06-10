77d54a468b3fd53c4fc8f2b35dde73e6704b9f57
Angie2005L <ingangie8@gmail.com>
2026-06-10 14:06:36 -0500

    commit-2

--- TallerUML/src/main/java/com/mycompany/talleruml/Curso.java
+++ TallerUML/src/main/java/com/mycompany/talleruml/Curso.java
@@ -5,5 +5,37 @@ public class Curso {
     private String id;
     private String nombre;
     private Boolean estadoOperativo;
+
+    public ActividadesSumativas getActividad_sumativa() {
+        return actividad_sumativa;
+    }
+
+    public void setActividad_sumativa(ActividadesSumativas actividad_sumativa) {
+        this.actividad_sumativa = actividad_sumativa;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Boolean getEstadoOperativo() {
+        return estadoOperativo;
+    }
+
+    public void setEstadoOperativo(Boolean estadoOperativo) {
+        this.estadoOperativo = estadoOperativo;
+    }
     
 }
