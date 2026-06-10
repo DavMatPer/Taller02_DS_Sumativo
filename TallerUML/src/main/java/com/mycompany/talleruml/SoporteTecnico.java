b28052266c5477f07d9df887def8158a9ee7afdb
David Matos Pérez <davmatos@espol.edu.ec>
2026-06-10 14:39:00 -0500

    Agregada clase de soporte Tecnico

---
+++ TallerUML/src/main/java/com/mycompany/talleruml/SoporteTecnico.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.talleruml;
+
+/**
+ *
+ * @author CltControl
+ */
+public class SoporteTecnico extends Usuario{
+
+    public SoporteTecnico(String usuario, String contrasena, String nombre, String apellido) {
+        super(usuario, contrasena, nombre, apellido);
+    }
+    
+    public void brindarAsistencia( Usuario usuario) {
+        
+    }
+            
+    public void resolverProblemaTecnico (Usuario usuario) {
+        
+    }
+            
+    public void derivarProblemaDeGestion( Administrador administrador) {
+        
+    }
+            
+    
+}
